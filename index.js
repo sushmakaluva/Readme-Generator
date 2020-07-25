9f981810bfc3ffb74073f23e319dc38a581a0f37
Sushma Kaluva <kaluvasushma@gmail.com>
2020-07-25 19:11:04 -0400

    added all the questions to index.js

--- index.js
+++ index.js
@@ -6,61 +6,85 @@ inquirer
     {
       type: 'input',
       message: 'Enter title',
-      name: '## Title',
+      name: 'Title',
     },
     {
       type: 'input',
-      message: 'Enter description',
-      name: '## Description',
+      message: 'Enter URL of the website',
+      name: 'URL',
     },
+
     {
-      type: 'list',
-      message: 'Enter table of contents',
-      name: '## Table of Contents',
-      choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions', 'Badge'],
+      type: 'input',
+      message: 'Enter description',
+      name: 'Description',
     },
     {
       type: 'input',
       message: 'Enter installation steps',
-      name: '## Installation',
+      name: 'Installation',
     },
     {
       type: 'input',
       message: 'Enter Usage',
-      name: '## Usage',
-    },
-    {
-      type: 'input',
-      message: 'Enter license details',
-      name: '## License',
+      name: 'Usage',
     },
     {
       type: 'input',
       message: 'Enter contributing factors',
-      name: '## Contributing',
+      name: 'Contributing',
     },
     {
       type: 'input',
       message: 'Enter tests to be done',
-      name: '## Tests',
+      name: 'Tests',
+    },
+    {
+      type: 'list',
+      message: 'Enter license details',
+      name: 'License',
+      choices: ['npm', 'PostgreSQL', 'Microsoft', 'Mozilla'],
+    },
+
+    {
+      type: 'list',
+      message: 'Enter table of contents',
+      name: 'Contents',
+      choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions', 'Badge'],
+
     },
     {
       type: 'input',
-      message: 'Enter the questions',
-      name: '## Questions',
+      message: 'Enter github username',
+      name: 'Username',
     },
     {
       type: 'input',
-      message: 'Badge details',
-      name: '## Badge',
+      message: 'Enter github username',
+      name: 'Email',
     },
+
+
 
   ])
   .then((response) => {
     console.log(response);
+    const { Title } = response;
+    const { URL } = response;
+    const { Description } = response;
+    const { Installation } = response;
+    const { Usage } = response;
+    const { Contributing } = response;
+    const { Tests } = response;
+    const { License } = response;
 
-    //  fs.writeFile('readme.md', JSON.stringify(response), (err) => {
-    //     if (err) throw err;
-    //     console.log('Saved!');
-    //   });
+    const { Username } = response;
+    const { Email } = response;
+    const { Contents } = response;
+
+
+    fs.writeFile('readme.md', `# Title\n\n${Title}\n\n## Website URL\n\n${URL}\n\n### Description\n\n${Description}\n\n### Table of Contents\n\n${Contents}\n\n### Installation\n\n${Installation}\n\n### Usage\n\n${Usage}\n\n### License\n\n![NPM](https://img.shields.io/npm/l/${License})\n\n### Contributing\n\n${Contributing}\n\n### Tests\n\n${Tests}\n\n### Questions\n\nIf any questions,reach out to me at ${Email}/n Github URL: https://github.com/${Username}/\n`, (err) => {
+      if (err) throw err;
+      console.log('Saved!');
+    });
   });
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Title
+
+Sushma
+
+## Website URL
+
+https://google.com
+
+### Description
+
+nothing
+
+### Table of Contents
+
+Questions
+
+### Installation
+
+no
+
+### Usage
+
+no
+
+### License
+
+![NPM](https://img.shields.io/npm/l/PostgreSQL)
+
+### Contributing
+
+no
+
+### Tests to be done
+
+no
+
+### Questions
+
+gnj
